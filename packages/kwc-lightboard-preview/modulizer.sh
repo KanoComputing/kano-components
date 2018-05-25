4bc8af581263719ccd98fb7497653ead30a7f7be
Paul Varache <paul@kano.me>
2018-05-25 11:46:45 +0100

    Module

---
+++ packages/kwc-lightboard-preview/modulizer.sh
@@ -0,0 +1,5 @@
+modulizer --out . \
+--npm-name @kano/kwc-lightboard-preview \
+--npm-version 3.0.0-beta.1 \
+--import-style name \
+--dependency-mapping web-components,@kano/web-components,3.0.0-beta.1
