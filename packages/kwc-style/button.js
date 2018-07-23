a9046dfb013daf104930e7d45960a731e5259006
Denis Dani <danidenis1995@gmail.com>
2018-07-23 16:42:24 +0100

    Add main button.js file

---
+++ packages/kwc-style/button.js
@@ -0,0 +1,39 @@
+import { button } from './button/base.js';
+import { black } from './button/black.js';
+import { disabled } from './button/disabled.js';
+import { ghost } from './button/ghost.js';
+import { outline } from './button/outline.js';
+import { primary } from './button/primary.js';
+import { secondary } from './button/secondary.js';
+import { tertiary } from './button/tertiary.js';
+import { transparent } from './button/transparent.js';
+import { warning } from './button/warning.js';
+import { white } from './button/white.js';
+
+export {
+    button,
+    black,
+    disabled,
+    ghost,
+    outline,
+    primary,
+    secondary,
+    tertiary,
+    transparent,
+    warning,
+    white,
+};
+
+export default {
+    button,
+    black,
+    disabled,
+    ghost,
+    outline,
+    primary,
+    secondary,
+    tertiary,
+    transparent,
+    warning,
+    white,
+};
