63034e39b3e7097a0433612935ffa52fd3da884d
Denis Dani <danidenis1995@gmail.com>
2018-07-24 14:18:10 +0100

    Add kwc-style.js

---
+++ packages/kwc-style/kwc-style.js.js
@@ -0,0 +1,18 @@
+import * as button from './button.js';
+import * as color from './color.js';
+import * as input from './input.js';
+import * as typography from './typography.js';
+
+export {
+    button,
+    color,
+    input,
+    typography,
+};
+
+export default {
+    button,
+    color,
+    input,
+    typography,
+};
